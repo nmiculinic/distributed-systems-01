07b37a23c0c191a8084ea524764561552b925856
Neven Miculinic <neven.miculinic@gmail.com>
2018-10-13 19:25:17 +0200

    Some initial stuff

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/nmiculinic/distributed-systems-01
+
+require github.com/spf13/cobra v0.0.3
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func main() {
+	send := &cobra.Command{
+		Use:   "send",
+		Short: "sends data",
+	}
+
+	recv := &cobra.Command{
+		Use:   "recv",
+		Short: "recv data",
+	}
+
+	root := &cobra.Command{
+		Use: "main",
+	}
+}
